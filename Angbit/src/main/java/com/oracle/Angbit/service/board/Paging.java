c11b5af200f63ab3197c56e80930b9eb3666397f
DESKTOP-AMV59HS\SEC <ryudy9658@gmail.com>
2021-11-29 16:09:42 +0900

    update

--- Angbit/src/main/java/com/oracle/Angbit/service/board/Paging.java
+++ Angbit/src/main/java/com/oracle/Angbit/service/board/Paging.java
@@ -5,9 +5,9 @@ import lombok.Setter;
 @Setter
 @Getter
 public class Paging {
-	private int currentPage = 1;	private int rowPage   = 10;
+	private int currentPage = 1;	private int rowPage   = 10;//한페이지 게시글수
 	private int pageBlock = 10;		
-	private int start;				private int end;
+	private int start;				private int end;           //게시글   
 	private int startPage;			private int endPage;
 	private int total;				private int totalPage;
 
@@ -21,7 +21,7 @@ public class Paging {
 		//밑에 페이지 목록
 		start = (currentPage - 1) * rowPage + 1;  // 시작시 1
 		end   = start + rowPage - 1;              // 시작시 10 
-		totalPage = (int) Math.ceil((double)total / rowPage);  // 시작시 2
+		totalPage = (int) Math.ceil((double)total / rowPage);  // 시작시 2, 소수점 이하 반올림
 		startPage = currentPage - (currentPage - 1) % pageBlock; // 시작시 1
 		endPage = startPage + pageBlock - 1;
 		if (endPage > totalPage) {
