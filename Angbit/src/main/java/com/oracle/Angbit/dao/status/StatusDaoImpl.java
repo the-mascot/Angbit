e4e862277deefe6895a84b26fc8e1c9a2ad9736c
Bae-kyoungmin <bgm91mp3@gmail.com>
2021-12-07 17:38:11 +0900

    [업데이트] 페이지네이션 완료 / 검색 예정

--- Angbit/src/main/java/com/oracle/Angbit/dao/status/StatusDaoImpl.java
+++ Angbit/src/main/java/com/oracle/Angbit/dao/status/StatusDaoImpl.java
@@ -60,105 +60,147 @@ public class StatusDaoImpl implements StatusDao {
 	}
 	
 	@Override
-	public List<TradeCoinInfo> allDateSort7(String id) {
-		List<TradeCoinInfo> allDateSort7 = session.selectList("AllDateSort7", id);
+	public List<TradeCoinInfo> allDateSort7(TradeCoinInfo trdCoin, String id) {
+		Map param = new HashMap();
+		param.put("trdCoin", trdCoin);
+		param.put("id", id);
+		List<TradeCoinInfo> allDateSort7 = session.selectList("AllDateSort7", param);
 		System.out.println("StatusDaoImpl allDateSort7.size() -> "+allDateSort7.size());
 		return allDateSort7;
 	}
 
 	@Override
-	public List<TradeCoinInfo> allDateSort30(String id) {
-		List<TradeCoinInfo> allDateSort30 = session.selectList("AllDateSort30", id);
+	public List<TradeCoinInfo> allDateSort30(TradeCoinInfo trdCoin, String id) {
+		Map param = new HashMap();
+		param.put("trdCoin", trdCoin);
+		param.put("id", id);
+		List<TradeCoinInfo> allDateSort30 = session.selectList("AllDateSort30", param);
 		System.out.println("StatusDaoImpl allDateSort30.size() -> "+allDateSort30.size());
 		return allDateSort30;
 	}
 
 	@Override
-	public List<TradeCoinInfo> allDateSort90(String id) {
-		List<TradeCoinInfo> allDateSort90 = session.selectList("AllDateSort90", id);
+	public List<TradeCoinInfo> allDateSort90(TradeCoinInfo trdCoin, String id) {
+		Map param = new HashMap();
+		param.put("trdCoin", trdCoin);
+		param.put("id", id);
+		List<TradeCoinInfo> allDateSort90 = session.selectList("AllDateSort90", param);
 		System.out.println("StatusDaoImpl allDateSort90.size() -> "+allDateSort90.size());
 		return allDateSort90;
 	}
 
 	@Override
-	public List<TradeCoinInfo> allDateSort180(String id) {
-		List<TradeCoinInfo> allDateSort180 = session.selectList("AllDateSort180", id);
+	public List<TradeCoinInfo> allDateSort180(TradeCoinInfo trdCoin, String id) {
+		Map param = new HashMap();
+		param.put("trdCoin", trdCoin);
+		param.put("id", id);
+		List<TradeCoinInfo> allDateSort180 = session.selectList("AllDateSort180", param);
 		System.out.println("StatusDaoImpl allDateSort180.size() -> "+allDateSort180.size());
 		return allDateSort180;
 	}
 
 //	체결내역(매수)
 	@Override
-	public List<TradeCoinInfo> buyComList(String id) {
+	public List<TradeCoinInfo> buyComList(TradeCoinInfo trdCoin, String id) {
 		System.out.println("StatusDaoImpl buyComList start...");
-		List<TradeCoinInfo> buycList = session.selectList("CBuyList", id);
+		Map param = new HashMap();
+		param.put("trdCoin", trdCoin);
+		param.put("id", id);
+		List<TradeCoinInfo> buycList = session.selectList("CBuyList", param);
 		return buycList;
 	}
 	
 	@Override
-	public List<TradeCoinInfo> buyDateSort7(String id) {
-		List<TradeCoinInfo> buyDateSort7 = session.selectList("buyDateSort7", id);
+	public List<TradeCoinInfo> buyDateSort7(TradeCoinInfo trdCoin, String id) {
+		Map param = new HashMap();
+		param.put("trdCoin", trdCoin);
+		param.put("id", id);
+		List<TradeCoinInfo> buyDateSort7 = session.selectList("buyDateSort7", param);
 		System.out.println("StatusDaoImpl buyDateSort7.size() -> "+buyDateSort7.size());
 		return buyDateSort7;
 	}
 
 	@Override
-	public List<TradeCoinInfo> buyDateSort30(String id) {
-		List<TradeCoinInfo> buyDateSort30 = session.selectList("buyDateSort30", id);
+	public List<TradeCoinInfo> buyDateSort30(TradeCoinInfo trdCoin, String id) {
+		Map param = new HashMap();
+		param.put("trdCoin", trdCoin);
+		param.put("id", id);
+		List<TradeCoinInfo> buyDateSort30 = session.selectList("buyDateSort30", param);
 		System.out.println("StatusDaoImpl buyDateSort30.size() -> "+buyDateSort30.size());
 		return buyDateSort30;
 	}
 
 	@Override
-	public List<TradeCoinInfo> buyDateSort90(String id) {
-		List<TradeCoinInfo> buyDateSort90 = session.selectList("buyDateSort90", id);
+	public List<TradeCoinInfo> buyDateSort90(TradeCoinInfo trdCoin, String id) {
+		Map param = new HashMap();
+		param.put("trdCoin", trdCoin);
+		param.put("id", id);
+		List<TradeCoinInfo> buyDateSort90 = session.selectList("buyDateSort90", param);
 		System.out.println("StatusDaoImpl buyDateSort90.size() -> "+buyDateSort90.size());
 		return buyDateSort90;
 	}
 
 	@Override
-	public List<TradeCoinInfo> buyDateSort180(String id) {
-		List<TradeCoinInfo> buyDateSort180 = session.selectList("buyDateSort180", id);
+	public List<TradeCoinInfo> buyDateSort180(TradeCoinInfo trdCoin, String id) {
+		Map param = new HashMap();
+		param.put("trdCoin", trdCoin);
+		param.put("id", id);
+		List<TradeCoinInfo> buyDateSort180 = session.selectList("buyDateSort180", param);
 		System.out.println("StatusDaoImpl buyDateSort180.size() -> "+buyDateSort180.size());
 		return buyDateSort180;
 	}
 
 //	체결내역(매도)
 	@Override
-	public List<TradeCoinInfo> sellComList(String id) {
+	public List<TradeCoinInfo> sellComList(TradeCoinInfo trdCoin, String id) {
 		System.out.println("StatusDaoImpl sellComList start...");
-		List<TradeCoinInfo> sellList = session.selectList("CSellList", id);
+		Map param = new HashMap();
+		param.put("trdCoin", trdCoin);
+		param.put("id", id);
+		List<TradeCoinInfo> sellList = session.selectList("CSellList", param);
 		return sellList;
 	}
 
 	@Override
-	public List<TradeCoinInfo> sellDateSort7(String id) {
+	public List<TradeCoinInfo> sellDateSort7(TradeCoinInfo trdCoin, String id) {
 		System.out.println("StatusDaoImpl sellDateSort7 start...");
-		List<TradeCoinInfo> sellDateSort7 = session.selectList("sellDateSort7", id);
+		Map param = new HashMap();
+		param.put("trdCoin", trdCoin);
+		param.put("id", id);
+		List<TradeCoinInfo> sellDateSort7 = session.selectList("sellDateSort7", param);
 		System.out.println("StatusDaoImpl sellDateSort7.size()->" + sellDateSort7.size());
 		return sellDateSort7;
 	}
 
 	@Override
-	public List<TradeCoinInfo> sellDateSort30(String id) {
+	public List<TradeCoinInfo> sellDateSort30(TradeCoinInfo trdCoin, String id) {
 		System.out.println("StatusDaoImpl sellDateSort30 start...");
-		List<TradeCoinInfo> sellDateSort30 = session.selectList("sellDateSort30", id);
+		Map param = new HashMap();
+		param.put("trdCoin", trdCoin);
+		param.put("id", id);
+		List<TradeCoinInfo> sellDateSort30 = session.selectList("sellDateSort30", param);
 		System.out.println("StatusDaoImpl sellDateSort30.size()->" + sellDateSort30.size());
 		return sellDateSort30;
 	}
 
 	@Override
-	public List<TradeCoinInfo> sellDateSort90(String id) {
+	public List<TradeCoinInfo> sellDateSort90(TradeCoinInfo trdCoin, String id) {
 		System.out.println("StatusDaoImpl sellDateSort90 start...");
-		List<TradeCoinInfo> sellDateSort90 = session.selectList("sellDateSort90", id);
+		Map param = new HashMap();
+		param.put("trdCoin", trdCoin);
+		param.put("id", id);
+		List<TradeCoinInfo> sellDateSort90 = session.selectList("sellDateSort90", param);
 		System.out.println("StatusDaoImpl sellDateSort90.size()->" + sellDateSort90.size());
 		return sellDateSort90;
 	}
 
 	@Override
-	public List<TradeCoinInfo> sellDateSort180(String id) {
+	public List<TradeCoinInfo> sellDateSort180(TradeCoinInfo trdCoin, String id) {
 		System.out.println("StatusDaoImpl sellDateSort180 start...");
-		List<TradeCoinInfo> sellDateSort180 = session.selectList("sellDateSort180", id);
+		Map param = new HashMap();
+		param.put("trdCoin", trdCoin);
+		param.put("id", id);
+		List<TradeCoinInfo> sellDateSort180 = session.selectList("sellDateSort180", param);
 		System.out.println("StatusDaoImpl sellDateSort180.size()->" + sellDateSort180.size());
 		return sellDateSort180;
 	}
@@ -175,6 +217,93 @@ public class StatusDaoImpl implements StatusDao {
 	public int total(String id) {
 		int tot = 0;
 		tot = session.selectOne("StatusTotal", id);
+		return tot;
+	}
+
+	@Override
+	public int total7(String id) {
+		int tot = 0;
+		tot = session.selectOne("StatusTotal7", id);
+		return tot;
+	}
+
+	@Override
+	public int total30(String id) {
+		int tot = 0;
+		tot = session.selectOne("StatusTotal30", id);
+		return tot;
+	}
+
+	@Override
+	public int total90(String id) {
+		int tot = 0;
+		tot = session.selectOne("StatusTotal90", id);
+		return tot;
+	}
+
+	@Override
+	public int total180(String id) {
+		int tot = session.selectOne("StatusTotal180", id);
+		return tot;
+	}
+
+	@Override
+	public int buyTotal(String id) {
+		int tot = session.selectOne("StatusBuyTotal", id);
+		return tot;
+	}
+
+	@Override
+	public int buyTotal7(String id) {
+		int tot = session.selectOne("StatusBuy7", id);
+		return tot;
+	}
+
+	@Override
+	public int buyTotal30(String id) {
+		int tot = session.selectOne("StatusBuy30", id);
+		return tot;
+	}
+
+	@Override
+	public int buyTotal90(String id) {
+		int tot = session.selectOne("StatusBuy90", id);
+		return tot;
+	}
+
+	@Override
+	public int buyTotal180(String id) {
+		int tot = session.selectOne("StatusBuy180", id);
+		return tot;
+	}
+
+	@Override
+	public int sellTotal(String id) {
+		int tot = session.selectOne("StatusSellTotal", id);
+		return tot;
+	}
+
+	@Override
+	public int sellTotal7(String id) {
+		int tot = session.selectOne("StatusSell7", id);
+		return tot;
+	}
+
+	@Override
+	public int sellTotal30(String id) {
+		int tot = session.selectOne("StatusSell30", id);
+		return tot;
+	}
+
+	@Override
+	public int sellTotal90(String id) {
+		int tot = session.selectOne("StatusSell90", id);
+		return tot;
+	}
+
+	@Override
+	public int sellTotal180(String id) {
+		int tot = session.selectOne("StatusSell180", id);
 		return tot;
 	}
 
