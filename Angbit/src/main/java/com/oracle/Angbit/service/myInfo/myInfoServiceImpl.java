ba0e559b9b510eb3aa1a13b966389dec7b5de79e
kimbad1992 <zlwmqhqk@naver.com>
2021-12-08 18:05:20 +0900

    [변경] 12-08 개인 자산 컬럼 추가 및 자산 업데이트 메서드 생성

--- Angbit/src/main/java/com/oracle/Angbit/service/myInfo/myInfoServiceImpl.java
+++ Angbit/src/main/java/com/oracle/Angbit/service/myInfo/myInfoServiceImpl.java
@@ -1,13 +1,9 @@
 package com.oracle.Angbit.service.myInfo;
 
-import com.oracle.Angbit.dao.invest.InvestDao;
 import com.oracle.Angbit.dao.myInfo.myInfoDao;
-import com.oracle.Angbit.model.common.CoinInfo;
 import com.oracle.Angbit.model.common.MemberInfo;
-import com.oracle.Angbit.service.invest.InvestService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.springframework.ui.Model;
 
 import java.util.List;
 
@@ -62,7 +58,23 @@ public class myInfoServiceImpl implements myInfoService {
 		midao.widraw(id);
 	}
 
+	@Override
 	public boolean chkWidraw(String id) {
 		return midao.chkWidraw(id);
+	}
+
+	@Override
+	public List getAllId() {
+		return midao.getAllId();
+	}
+
+	@Override
+	public List getAllCoincode(String id) {
+		return midao.getAllCoincode(id);
+	}
+
+	@Override
+	public void updateAsset(String id, int asset) {
+		midao.updateAsset(id, asset);
 	}
 }
