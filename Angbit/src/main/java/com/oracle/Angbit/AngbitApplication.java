0ec2f9bf12d7726d136658153b655b47d9aa8766
kimbad1992 <zlwmqhqk@naver.com>
2021-12-13 21:40:31 +0900

    [변경] 12-13 GRADLE BUILD용

--- Angbit/src/main/java/com/oracle/Angbit/AngbitApplication.java
+++ Angbit/src/main/java/com/oracle/Angbit/AngbitApplication.java
@@ -2,12 +2,18 @@ package com.oracle.Angbit;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class AngbitApplication {
+public class AngbitApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(AngbitApplication.class, args);
 	}
 
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return super.configure(builder);
+	}
 }
