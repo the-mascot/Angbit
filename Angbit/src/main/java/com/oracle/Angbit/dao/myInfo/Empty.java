c5ee79e2da60b1bc91a7f3dfdfb36a44c6654ca6
DESKTOP-548L1U1\mascot <dmstn1812@naver.com>
2022-02-15 16:26:45 +0900

    롤백완료

--- Angbit/src/main/java/com/oracle/Angbit/dao/myInfo/Empty.java
+++
@@ -1,5 +0,0 @@
-package com.oracle.Angbit.dao.myInfo;
-
-public class Empty {
-
-}
--- Angbit/src/main/java/com/oracle/Angbit/service/myInfo/userDetailsServiceImpl.java
+++
@@ -1,9 +0,0 @@
-package com.oracle.Angbit.service.myInfo;
-
-import org.springframework.security.core.userdetails.UserDetailsService;
-
-public interface userDetailsServiceImpl extends UserDetailsService {
-	
-	//public UserDetailsVO loadUserByUsername();
-	
-}
